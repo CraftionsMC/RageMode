8fe846a67a227693af118c76a05b1cf89f541ac3
Ben Siebert <dev@mctzock.de>
2021-05-29 08:23:48 +0200

    Added SetSpawn command

---
+++ src/main/java/net/craftions/ragemode/commands/CommandSetSpawn.java
@@ -0,0 +1,17 @@
+/**
+ * @copyright (c) 2021 Ben Siebert. All rights resered.
+ * @author Ben Siebert
+ */
+package net.craftions.ragemode.commands;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+
+public class CommandSetSpawn implements CommandExecutor {
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        return true;
+    }
+}

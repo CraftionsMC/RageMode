d7df7d8b5851a15abe09231b29e309e712cc89bc
Ben Siebert <dev@mctzock.de>
2021-05-29 08:46:58 +0200

    Set Death Message

--- src/main/java/net/craftions/ragemode/events/EventPlayerDeath.java
+++ src/main/java/net/craftions/ragemode/events/EventPlayerDeath.java
@@ -4,6 +4,7 @@
  */
 package net.craftions.ragemode.events;
 
+import net.craftions.ragemode.RageMode;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.PlayerDeathEvent;
@@ -12,6 +13,7 @@ public class EventPlayerDeath implements Listener {
 
     @EventHandler
     public void onDeath(PlayerDeathEvent e){
+        e.setDeathMessage(RageMode.prefix + "§c" + e.getEntity().getName() + " §7was killed");
         e.getDrops().clear();
     }
 }
